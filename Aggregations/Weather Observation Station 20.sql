0e03be46d0d3567ba9395e998ffdf5c606a52b71
Hasika <hasika20.2005@gmail.com>
2024-04-07 17:07:39 +0530

    Weather Observation Station 20

--- Aggregations/Weather Observation Station 20.sql
+++ Aggregations/Weather Observation Station 20.sql
@@ -1,4 +1,8 @@
---Question LInk: https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+--Question Link: https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
 
-select ROUND(AVG(POPULATION))
-from CITY;
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY LAT_N), 4) AS Median
+FROM STATION;
