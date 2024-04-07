9d8ba3e95625ee02593c0546eda078f7fb4b9f0e
Hasika <hasika20.2005@gmail.com>
2024-04-07 16:55:33 +0530

    Weather Observation Station 14

---
+++ Aggregations/Weather Observation Station 14.sql
@@ -0,0 +1,7 @@
+--Question LInk: https://www.hackerrank.com/challenges/weather-observation-station-14/problem?isFullScreen=true
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select round(max(lat_n), 4) from station where lat_n<137.2345;
